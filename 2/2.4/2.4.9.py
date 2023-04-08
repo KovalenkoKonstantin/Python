7a46db5176d5e5818c47e2ba2514a03e1d3f3502
Kos <kovalenkorjc@gmail.com>
2023-04-08 18:09:37 +0300

    Update

---
+++ 2/2.4/2.4.9.py
@@ -0,0 +1,6 @@
+# Напиши программу,
+# которая будет считывать 5 строк текста
+# и выводить их в консоль (на разных строках)
+print(input(), '\n', input(), '\n', input(), '\n', input(), '\n', input(), sep='')
+print(input() + '\n' + input() + '\n' + input() + '\n' + input(), '\n' + input())
+print(input(), input(), input(), input(), input(), sep="\n")
