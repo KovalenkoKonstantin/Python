6e1bd64409c7ce9c72c307673a931a321e4a8015
Kos <kovalenkorjc@gmail.com>
2023-04-08 18:23:37 +0300

    Update

---
+++ 2/2.5/2.5.4.py
@@ -0,0 +1,8 @@
+# Это неработающее решение Сакуры в этой задаче.
+# Прокомментируйте как-нибудь это безобразие (код должен делать ту-же дичь,
+# что и до изменений, нужно только добавить комментарии. Поругайте ее за этот код.)
+pervaia_strochka=  input();vtoraya_strochka=input();chetviortaia_strochka      =     input();
+tretia_strochka =     input();pataia_strochka     =    input();
+
+print(pervaia_strochka,vtoraya_strochka,tretia_strochka,chetviortaia_strochka,  sep = 'n');
+print(pataia_strochka);
