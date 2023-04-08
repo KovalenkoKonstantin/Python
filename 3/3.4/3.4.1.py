9f67dc771222b8c50c9e2b92d778159150644d12
Kos <kovalenkorjc@gmail.com>
2023-04-08 19:31:18 +0300

    Update

---
+++ 3/3.4/3.4.1.py
@@ -0,0 +1,22 @@
+# Строки тоже можно складывать друг с другом
+a = "Hello"
+b = " world"
+c = a + b
+print(c)
+
+print("5" + "10")
+
+# print(10 + "5")
+
+# print(Hello_world + 5)
+
+Hello_world = 5
+print(Hello_world + 5)
+
+print("+"+"+")
+
+print('+\'+\'+')
+
+print('+\''+'\'+')
+
+# print("10"-"0")
