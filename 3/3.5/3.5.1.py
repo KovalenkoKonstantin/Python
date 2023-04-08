5108e670a05feda4443cbe451baeebf35fc0ef3c
Kos <kovalenkorjc@gmail.com>
2023-04-08 21:51:37 +0300

    Update

---
+++ 3/3.5/3.5.1.py
@@ -0,0 +1,21 @@
+s = "строка "
+print(s * 10)
+
+a = 10
+print("a" * 5)
+
+a = 10
+print(a * 5)
+
+a = "10"
+print(a * 5)
+
+a = 10
+# print("a" * "5")
+
+a = 10
+print(a * "5")
+
+print("#" * 0)
+
+print("#" * -3)
