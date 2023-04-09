059346d2d4b4bc9925899acc1e040831e5f69fbc
Kos <kovalenkorjc@gmail.com>
2023-04-09 23:06:15 +0300

    Update

---
+++ 3/3.11/3.11.2.py
@@ -0,0 +1,20 @@
+x = 1.234e+3
+print(x)
+
+print(float(10_000_000_000_000_000))
+
+print(float(1_000_000_000_000_000))
+
+print(float(10 ** -5))
+
+print(float(10 ** -4))
+
+print(float(1.8 * 10 ** 308))
+
+# print(float(10 ** 309))
+
+print(float(10 ** 308))
+
+x = 10 ** 309
+print(x)
+
