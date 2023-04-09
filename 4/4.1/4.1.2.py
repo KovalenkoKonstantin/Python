30710dff8f35e4c4ae833f89460ec9b6927ab1f6
Kos <kovalenkorjc@gmail.com>
2023-04-10 00:34:19 +0300

    Update

---
+++ 4/4.1/4.1.2.py
@@ -0,0 +1,38 @@
+a = 10
+b = 10
+if a == b:
+    print("a и b равны")
+else:
+    print("a и b не равны")
+
+a = 10
+b = 10
+if a != b:
+    print("a и b не равны")
+else:
+    print("a и b равны")
+
+if ("hello world" == "Hello world"):
+    print("YES")
+else:
+    print("NO")
+
+hello_world = "hello_world"
+if('hello_world' == hello_world):
+    print("YES")
+else:
+    print("NO")
+
+a = 10
+b = '10'
+if a == b:
+    print("a и b равны")
+else:
+    print("a и b не равны")
+
+# a = 5
+# b = '5'
+# if a > b:
+#     print("a больше чем b")
+# else:
+#     print("a не больше чем b")
