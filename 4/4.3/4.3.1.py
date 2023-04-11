1d7be7fd98a8b8b6b53db7df87792c0cdcafbc12
Kos <kovalenkorjc@gmail.com>
2023-04-11 22:10:38 +0300

    Update

---
+++ 4/4.3/4.3.1.py
@@ -0,0 +1,15 @@
+a = 10
+if a > 5 and a % 2 == 0:
+    print("a - четное число больше 5")
+
+n = 5
+if n == 5 and n == 7:
+    print("YES")
+else:
+    print("NO")
+
+    n = 5
+    if n == 5 or n == 7:
+        print("YES")
+    else:
+        print("NO")
---
+++ 4/4.3/4.3.3.py
@@ -0,0 +1,10 @@
+a = 5
+if not a < 2:
+    print("a >= 2")
+else:
+    print("a < 2")
+
+if not 3 > 5:
+    print("YES")
+else:
+    print("NO")
