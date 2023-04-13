32340a5e3dd04e3133adbc11e9510406dfbdcca4
Kos <kovalenkorjc@gmail.com>
2023-04-13 20:02:35 +0300

    Update

---
+++ 4/4.6/4.6.1.py
@@ -0,0 +1,31 @@
+print(int(True) * 5)
+
+print(True ** False, False ** True)
+
+print(False ** bool(str(False)), True ** bool(str(True)))
+
+print(str(True) * 3)
+
+print(True * str(3))
+
+print(True * True * str(False))
+
+print(max(True, False))
+
+print(max(True, 1), max(1, True))
+
+print(bool())
+
+print("int", "or" == "or", "int")
+
+print(int(bool(str(bool()))))
+
+print(True + bool("False") + False + bool("True"))
+
+a = 3
+print(len(True * a * str(a) + "a" * bool()))
+
+print("or" > "bool" and bool("and") and bool("or"))
+
+print((bool("bool('and')") and bool()) or bool(len("")))
+
