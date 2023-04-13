b844d2a659d96453575b6a1a7e2c462c23bc573a
Kos <kovalenkorjc@gmail.com>
2023-04-13 21:42:40 +0300

    Update

---
+++ 4/4.7/4.7.1.py
@@ -0,0 +1,30 @@
+_ = str(25_0_0_0)
+_ = _
+_ += str(int(True))
+print(_)
+
+a = (len("\\\'") == 4 and len("\/") == 2)
+print(a and bool(" "))
+
+print(True or True and False)
+
+print(6 > 5 + 5 > 2)
+
+print((6 > 5) + 5 > 2)
+
+print(6 > 5 + (5 > 2))
+
+print((6 > 5) + (5 > 2))
+
+print(True > False)
+
+print(True > 0.9)
+
+print(2 > True >= False + True > (False > 1))
+
+print(min(False, True) + max(False, True))
+
+print(min("False", "True") + max("False", "True"))
+
+print("c" < "с")
+
