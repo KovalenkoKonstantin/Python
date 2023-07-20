60eadadf16f55b245bdbf4dc68c4db1490fea4b3
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-07-20 11:05:46 +0300

    Update

---
+++ 7/7.3/7.3.4.py
@@ -0,0 +1,7 @@
+s = "hello\nworld"
+print(list(s))
+
+print(list(range(1, 10)))
+# Функция range может принимать 3 числа - start, end и step,
+# тогда range будет перебирать числа от start (включительно)
+# до end (не включительно) с шагом, равным step
