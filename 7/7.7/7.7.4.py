ac666ec3055aa25867987b6d27807d254c5ed99c
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-07-31 10:12:49 +0300

    Update

---
+++ 7/7.7/7.7.4.py
@@ -0,0 +1,12 @@
+# Что выведет этот код?
+
+l = [1, 2, 3, 4, 5]
+# print(l.find(4))
+print(l.index(4))
+
+l = [1, 2, 3, 4, 5]
+print(l.index(4))
+
+l = ['1', '2', '3', '4', '5']
+# print(l.index(4))
+print(l.index('4'))
